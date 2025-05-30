907feaa6d2f3ba1915d7112837a2a8feadd4479d
nntnds <jasavchuk08@gmail.com>
2025-05-30 15:58:27 +0400

    check movie id from database

--- app/src/main/java/com/example/application/screens/DetailScreen.kt
+++ app/src/main/java/com/example/application/screens/DetailScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
